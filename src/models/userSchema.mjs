02c71c2e0b03e8aa98006898eea9e7aab4a314e1
showszs <pasativan152@gmail.com>
2026-08-15 00:07:34 +0300

    refactor: simplify password hashing logic in userSchema pre-save hook

--- src/models/userSchema.mjs
+++ src/models/userSchema.mjs
@@ -12,15 +12,11 @@ const userSchema = new mongoose.Schema(
     }
 )
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
     if (this.isModified('password')) {
-        try {
-            const salt = await bcrypt.genSalt(10)
-            this.password = await bcrypt.hash(this.password, salt)
-        } catch (err) {
-            return next(err)
-        }
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
     }
-    next()
 })
+
 export const User = mongoose.model('User', userSchema)
